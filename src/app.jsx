8a8493c88b4512aeae7709090b8a3f79ab51c617
mousehacks <77026371+mousehacks@users.noreply.github.com>
2021-02-08 22:43:00 +0100

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./list";
+import Heading from "./heading";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
